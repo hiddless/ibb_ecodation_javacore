f8d581e1c2f221728892370c2bc7dea0b0c5d6ab
hiddless <164763754+hiddless@users.noreply.github.com>
2025-02-08 18:19:10 +0300

    wua

---
+++ src/main/java/com/hiddless/_1_part_javatypes/_09_Answers.java
@@ -0,0 +1,33 @@
+package com.hiddless._1_part_javatypes;
+
+import java.util.Scanner;
+
+public class _09_Answers {
+    public static void main(String[] args) {
+
+        //İlk sorunun cevabı
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcılardan a ve b değerlerini almak için
+        System.out.println("a değerini girin: ");
+        double a = scanner.nextDouble();
+
+        System.out.println("b değerini girin: ");
+        double b = scanner.nextDouble();
+
+        // ax + b denklemini hesaplama
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Denklemin sonsuz çözümü var.");
+            } else {
+                System.out.println("Denklemin çözümü yok");
+            }
+        } else {
+            double x = -b / a ;
+            System.out.println("Denklemin çözümü: x = " + x);
+            }
+        scanner.close();
+        }
+    }
+
