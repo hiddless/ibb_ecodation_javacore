f7552cd5782fddd736eda4f66771c47e1941fb60
hiddless <164763754+hiddless@users.noreply.github.com>
2025-02-11 19:07:56 +0300

    wua

---
+++ src/main/java/com/hiddless/_1_part_javatypes/_2_week/_12_loop_dowhile.java
@@ -0,0 +1,16 @@
+package com.hiddless._1_part_javatypes._2_week;
+
+public class _12_loop_dowhile {
+    public static void main(String[] args) {
+        // i++
+        // i=i+1
+        // i+=1
+            int i = 32;
+        do {
+            System.out.println(i + " - ");
+            i++;
+        } while (i <= 10);
+    }
+}
+
+
