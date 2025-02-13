a54ece42bd4fd9f612be10af473ea95627e31d98
hiddless <164763754+hiddless@users.noreply.github.com>
2025-02-13 20:57:21 +0300

    wua

--- src/main/java/com/hiddless/_1_part_javatypes/_2_week/_15_Cast.java
+++ src/main/java/com/hiddless/_1_part_javatypes/_2_week/_15_Cast.java
@@ -22,7 +22,23 @@ public class _15_Cast {
         char ascii2= (char) cast3Int;
         System.out.println(cast3Int+"ascii karşılığı: "+ascii2);
 
-        // 4-)
+        // 4-) String => int
+        String castString4="10";
+        int cast4Int=Integer.valueOf(castString4);
+        int cast4Int2=Integer.parseInt(castString4);
+        System.out.println(cast4Int2+20);
+        System.out.println(castString4+20);
+        System.out.println(cast4Int+20);
+
+        // 5-) Int ==> String
+        int cast5Int=45;
+        String cast5String1=String.valueOf(cast5Int);
+        String cast5String2=cast5String1.toString();
+        System.out.println(cast5String1);
+        System.out.println(cast5String2);
+
+
+
 
     }
 }
